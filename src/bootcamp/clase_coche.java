15c4bc1676cf64fcc79fdc214711006803ffbfeb
oscar706 <paucaraoscar706@gmail.com>
2023-04-13 13:32:15 -0400

    update new projects

---
+++ src/bootcamp/clase_coche.java
@@ -0,0 +1,4 @@
+package bootcamp;
+
+public class clase_coche {
+}
---
+++ src/bootcamp/suma3numeros.java
@@ -0,0 +1,14 @@
+package bootcamp;
+
+public class suma3numeros {
+    public static void main(String[] args) {
+        suma(10, 12, 20);
+    }
+    public static void suma(int num1, int num2, int num3){
+        int resultado;
+        resultado = num1 + num2 + num3;
+
+        System.out.println(resultado);
+
+    }
+}
